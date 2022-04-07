2954588d38d9342c25d60371d3f02e517df1dc6f
Son Hong Do <sondh0127@gmail.com>
2022-04-07 22:06:45 +0700

    handle dynamic input

--- src/api/handler.ts
+++ src/api/handler.ts
@@ -1,10 +1,11 @@
-import type { Handler } from 'vite-plugin-mix'
 import type { Message } from 'discord.js-selfbot-v13'
 import { Client, Collection } from 'discord.js-selfbot-v13'
+import express from 'express'
 
-const RECEIVED_CHANNEL_ID = '960927218769473618'
-const PREFIX = 'ms-'
+const app = express()
+app.use(express.json())
 const client = new Client()
+const PREFIX = 'ms-'
 const commands = new Collection<string, Function>()
 
 function ping(message: Message) {
@@ -12,90 +13,100 @@ function ping(message: Message) {
   return message.channel.send('Pong!')
 }
 
-function pingOther(message: Message) {
-  // send a message to the channel the message was sent in
-  return message.channel.send('Pong!')
-}
-
 commands.set('ping', ping)
-commands.set('pingOther', pingOther)
 
-client.on('ready', async() => {
-  // log the user tag that is logged in to the console
-  console.log(`${client.user.username} is ready!`)
+app.get('/user', (req, res) => {
+  res.json(client.user)
 })
 
-client.on('messageCreate', async(message) => {
-  console.log('[LOG] ~ file: main.ts ~ line 76 ~ message', message)
+app.post('/start', (req, res) => {
+  console.log('[LOG] ~ file: handler.ts ~ line 24 ~ req.body', req.body)
+  const { channels, mainChannel, token } = req.body
+  client.on('ready', async() => {
+    // log the user tag that is logged in to the console
+    console.log(`${client.user.username} is ready!`)
+    res.json(client.user)
+  })
 
-  // check if the message is not empty
-  if (message.content) {
-    // if the message not come from RECEIVED_CHANNEL_ID
-    if (message.channel.id !== RECEIVED_CHANNEL_ID) {
-      const channel = client.channels.cache.get(RECEIVED_CHANNEL_ID)
+  client.on('messageCreate', async(message) => {
+    console.log('[LOG] ~ file: main.ts ~ line 76 ~ message.content', message.content)
 
-      setTimeout(
-        () => channel.send(message.content),
-        1000 + Math.floor(Math.random() * 1000),
-      )
-    }
+    // check if the message is not empty
+    if (message.content) {
+      // regex to check a string is contains a link
+      const regex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi
+      const isLink = regex.test(message.content)
+      console.log('[LOG] ~ file: handler.ts ~ line 39 ~ isLink', isLink)
 
-    // check if the message starts with the prefix from the .env file
-    if (message.content.startsWith(PREFIX)) {
-      // if it does, then get the command from the message
-      // and remove the prefix from the message
-      const command = message.content.slice(PREFIX.length).split(/ +/g)[0]
-      // get the arguments from the message
-      const args = message.content.slice(PREFIX.length).split(/ +/g).slice(1)
+      // if the message not come from mainChannel
+      const isNotMainChannel = message.channel.id !== mainChannel
+      // message is coming from source channel
+      const fromSourceChannels = channels.includes(message.channel.id)
 
-      // check if the command is in the commands collection
-      if (commands.has(command)) {
-        // check if the command is a function
-        if (typeof commands.get(command) === 'function') {
-          // if it is, then bind the function to the client
-          // first, use try catch to catch any errors
-          try {
-            // before calling the command, send typing to the channel
-            await message.channel
-              .sendTyping()
-              .then(() => {
-                // wait for 1 seconds before calling the command
-                // plus a random millisecond to make it look more natural to the user
-                // and pass the message and arguments as arguments
-                setTimeout(
-                  () => commands.get(command)(message, args),
-                  1000 + Math.floor(Math.random() * 1000),
-                )
-              })
-              .then(() => {
-                // after the command is called, log the command
-                console.log(`${message.author.tag} (${message.author.id}) ran the command ${command}`)
-              })
+      if (isNotMainChannel && fromSourceChannels && isLink) {
+        const channel = client.channels.cache.get(mainChannel)
+
+        setTimeout(
+          () => channel.send(message.content),
+          1000 + Math.floor(Math.random() * 1000),
+        )
+      }
+
+      // check if the message starts with the prefix from the .env file
+      if (message.content.startsWith(PREFIX)) {
+        // if it does, then get the command from the message
+        // and remove the prefix from the message
+        const command = message.content.slice(PREFIX.length).split(/ +/g)[0]
+        // get the arguments from the message
+        const args = message.content.slice(PREFIX.length).split(/ +/g).slice(1)
+
+        // check if the command is in the commands collection
+        if (commands.has(command)) {
+          // check if the command is a function
+          if (typeof commands.get(command) === 'function') {
+            // if it is, then bind the function to the client
+            // first, use try catch to catch any errors
+            try {
+              // before calling the command, send typing to the channel
+              await message.channel
+                .sendTyping()
+                .then(() => {
+                  // wait for 1 seconds before calling the command
+                  // plus a random millisecond to make it look more natural to the user
+                  // and pass the message and arguments as arguments
+                  setTimeout(
+                    () => commands.get(command)(message, args),
+                    1000 + Math.floor(Math.random() * 1000),
+                  )
+                })
+                .then(() => {
+                  // after the command is called, log the command
+                  console.log(`${message.author.tag} (${message.author.id}) ran the command ${command}`)
+                })
+            }
+            catch (error) {
+              console.error(`Error: ${error}`)
+            }
+
+            // if the command is not a function
           }
-          catch (error) {
-            console.error(`Error: ${error}`)
+          else {
+            console.error(`Error: ${command} is not a function`)
           }
-
-          // if the command is not a function
         }
         else {
-          console.error(`Error: ${command} is not a function`)
+          console.error(`Error: ${command} is not a command`)
         }
       }
-      else {
-        console.error(`Error: ${command} is not a command`)
-      }
     }
-  }
+  })
+
+  client.login(token)
 })
 
-const token = 'MzUwNTc3NjgyMDM3MDgwMDY1.YkxXvA._vSKbZ7JDOU53IiHXX7hOgxgi0c'
+app.post('/stop', (req, res) => {
+  client.destroy()
+  res.json({ message: 'stopped' })
+})
 
-client.login(token)
-
-export const handler: Handler = (req, res, next) => {
-  if (req.path === '/hi')
-    return res.end('hello')
-
-  next()
-}
+export const handler = app
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,55 @@
 <script setup lang="ts">
-const servers = $ref([])
+import { lyla } from 'lyla'
+const channelRegex = /https:\/\/discord.com\/channels\/*.*\/(.*)/
 
-function login() {
-  console.log('[LOG] ~ file: index.vue ~ line 3 ~ servers', servers)
+const token = $ref('MzUwNTc3NjgyMDM3MDgwMDY1.YkxXvA._vSKbZ7JDOU53IiHXX7hOgxgi0c')
+const mainChannel = $ref('https://discord.com/channels/960915142042845284/960915142588137614') // Test1
+const channels = $ref(
+  [
+    'https://discord.com/channels/960927218291310645/960927218769473618', // Test2
+    'https://discord.com/channels/961627753378091079/961627753982083124', // Test3
+  ],
+)
+
+let isLoading = $ref(false)
+let user = $ref<any>(null)
+
+onMounted(async() => {
+  await getUser()
+})
+
+async function getUser() {
+  isLoading = true
+  const { json } = await lyla.get('/user')
+  user = json
+  isLoading = false
 }
 
-const canSubmit = $computed(() => servers.some(item => item))
+async function login() {
+  isLoading = true
+  const _channels = channels.map(channel => channelRegex.exec(channel)?.[1])
+  const _mainChannel = channelRegex.exec(mainChannel)?.[1]
+  const { json } = await lyla.post('/start', {
+    json: {
+      token,
+      channels: _channels,
+      mainChannel: _mainChannel,
+    },
+  })
+  user = json
+  isLoading = false
+}
+
+async function stop() {
+  isLoading = true
+  const { json } = await lyla.post('/stop', {
+    json: {},
+  })
+  isLoading = false
+  user = null
+}
+
+const canSubmit = $computed(() => channels.some(item => item))
 
 </script>
 
@@ -15,21 +59,59 @@ const canSubmit = $computed(() => servers.some(item => item))
     <p>
       <a rel="noreferrer" href="https://web-minterscan.vercel.app" target="_blank">MinterScan</a>
     </p>
-    <p>
-      <em text-sm op75>MinterScan SelfBot</em>
-    </p>
-    {{ name }}
 
-    <div py-4 flex="~ col" items="center" justify="center" gap="10px">
-      <InputField v-model:value="servers[0]" placeholder="Server 1 ID" />
-      <InputField v-model:value="servers[1]" placeholder="Server 2 ID" />
-      <InputField v-model:value="servers[2]" placeholder="Server 3 ID" />
+    <div class="flex my-10 gap-4 justify-center">
+      <div class="flex flex-col">
+        <div py-4 flex="~ col" items="center" justify="center" gap="10px">
+          <em text-sm op75>Token</em>
+          <InputField v-model:value="token" placeholder="Selfbot Token" />
+        </div>
+
+        <div py-4 flex="~ col" items="center" justify="center" gap="10px">
+          <em text-sm op75>Destination Channels</em>
+          <InputField v-model:value="mainChannel" placeholder="Channel Link" />
+        </div>
+      </div>
+
+      <div py-4 flex="~ col" items="center" justify="center" gap="10px">
+        <em text-sm op75>Source Channels</em>
+        <InputField v-model:value="channels[0]" placeholder="Channel 1 Link" />
+        <InputField v-model:value="channels[1]" placeholder="Channel 2 Link" />
+        <InputField v-model:value="channels[2]" placeholder="Channel 3 Link" />
+      </div>
     </div>
-
     <div>
-      <button class="m-3 text-sm btn" :disabled="!canSubmit" @click="login">
-        Login
-      </button>
+      <div v-if="!user">
+        <button class="m-3 text-sm btn" :disabled="!canSubmit" @click="login">
+          <div class="flex items-center gap-2">
+            <div
+              v-if="isLoading"
+              i-carbon:center-circle
+              text-normal
+              inline-block
+              animate-spin
+              animate-3s
+              preserve-3d
+            />
+            <span v-else>Login</span>
+          </div>
+        </button>
+      </div>
+      <div v-else>
+        <div class="m-3 text-sm">
+          Running!
+        </div>
+        <div class="flex items-center gap-4 justify-center">
+          <img class="rounded-full w-18 h-18" :src="user.avatarURL">
+          <div class="text-sm font-medium">
+            <p>{{ user.tag }}</p>
+            <p>{{ user.emailAddress }}</p>
+          </div>
+        </div>
+        <button class="m-3 text-sm btn" :disabled="isLoading" @click="stop">
+          Stop
+        </button>
+      </div>
     </div>
   </div>
 </template>
